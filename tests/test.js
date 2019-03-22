9e740548fa5dc68325ab608cb738843f68c08056
kkghai <kaghai@microsoft.com>
2019-03-22 14:36:39 +0530

    More tests added

--- tests/test.js
+++ tests/test.js
@@ -20,6 +20,59 @@ describe('String', function () {
           //  assert.equal(-1, myString.indexOf('e'));
         });
         }
+                if (0==1) {
+
+        	it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
+
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+          //  assert.equal(-1, myString.indexOf('e'));
+        });
+        }
+                if (0==1) {
+
+        	it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
+
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+          //  assert.equal(-1, myString.indexOf('e'));
+        });
+        }
+                if (0==1) {
+
+        	it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
+
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+          //  assert.equal(-1, myString.indexOf('e'));
+        });
+        }
+                if (0==1) {
+
+        	it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
+
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+          //  assert.equal(-1, myString.indexOf('e'));
+        });
+        }
+
+    });
+});
+
+describe('String', function () {
+    describe('#indexOf', function () {
+        it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
+
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+          //  assert.equal(-1, myString.indexOf('e'));
+        });
 
     });
 });
